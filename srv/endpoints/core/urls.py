db0bd2a38f39d6629b4f53109da52c19b71134d0
dluman <dluman@allegheny.edu>
2024-06-02 20:06:27 -0400

    Removing extra semantic step for climate API

--- srv/endpoints/core/urls.py
+++ srv/endpoints/core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, re_path, include
 from rest_framework.reverse import reverse
 
 urlpatterns = [
-    re_path(r'^v1/climate/all', include(('climate.urls','climate'), namespace='v1'))
+    re_path(r'^v1/climate', include(('climate.urls','climate'), namespace='v1'))
 ]
