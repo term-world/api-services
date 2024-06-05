52285855771d9b6279ecaf8e6f59023662d23267
dluman <dluman@allegheny.edu>
2024-06-05 15:48:43 -0400

    First use of Swagger UI; how can we customize?

--- srv/endpoints/core/urls.py
+++ srv/endpoints/core/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path, re_path, include
 from rest_framework.reverse import reverse
-from rest_framework_swagger.views import get_swagger_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-schema_view = get_swagger_view(title='Climate API')
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Climate API",
+      default_version='v1',
+      description="Pass-through for location-specific OpenWeatherAPI data.",
+      terms_of_service="",
+      contact=openapi.Contact(email=""),
+      license=openapi.License(name=""),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('v1/climate/', include('climate.urls')),
-    path('v1/climate/docs/', schema_view)
+    path('v1/climate/docs/', schema_view.with_ui('swagger', cache_timeout = 0),name = 'Climate API documentation.')
 ]
