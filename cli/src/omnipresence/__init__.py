eb31be3c704254dc24fd6dd6e6c8d599b5b06b9d
dluman <dluman@allegheny.edu>
2024-06-07 12:38:41 -0400

    Updating client-side library; need to add Github env variables

--- cli/src/omnipresence/__init__.py
+++ cli/src/omnipresence/__init__.py
@@ -1,5 +1,17 @@
 import os
 import requests
 
+from getpass import getuser
+from dotenv import load_dotenv()
+load_dotenv()
+
 def main():
-    
+    response = requests.post(
+        os.getenv('API_URL'),
+        data = {
+            "username": getuser(),
+            "charname": getuser(),
+            "working_dir": os.getcwd()
+        }
+    )
+
