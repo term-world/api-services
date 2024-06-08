db9d11266bceb3e7d9830b2476832d2f1f51d36f
Danniyb <bekele01@allegheny.edu>
2024-06-08 17:27:21 -0400

    defaults

--- srv/endpoints/inventory/serializers.py
+++ srv/endpoints/inventory/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from .models import Inventory
 
 class InventorySerializer(serializers.ModelSerializer):
+    item_qty = serializers.FloatField(default=1.0)
+    item_weight = serializers.FloatField(default=1.0)
+    item_bulk = serializers.FloatField(default=1.0)
+    item_consumable = serializers.BooleanField(default=False)
+
     class Meta:
         model = Inventory
         fields = '__all__'
