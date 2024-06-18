8988dfefba3a721e3023a0503fc58218625927de
dluman <dluman@allegheny.edu>
2024-06-18 10:14:05 -0400

    More tracing to check on query status

--- srv/endpoints/inventory/views.py
+++ srv/endpoints/inventory/views.py
@@ -197,12 +197,12 @@ class GiveInventoryView(GenericAPIView, UpdateModelMixin):
         qty = 1
         weight = getattr(item, 'item_weight')
         if not created: # Some amount already existed in receiver's inventory
+            print(qty)
             qty = getattr(given_item, 'item_qty') + 1
         # Set properties of given record to reflect actual amounts, bulk
         # TODO: Reject if amount given is greater than space available -- this is a trigger
         item_params['item_qty'] = qty
         item_params['item_bulk'] = qty * weight
-        print(item_params)
         if created:
             # In the case that the item is created, let's set up the whole record
             for param in item_params:
