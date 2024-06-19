cd45cd85bea32ce6e3731cd5caf2dcfbd914fcf4
dluman <dluman@allegheny.edu>
2024-06-19 11:36:33 -0400

    Updating with client interaction with who command

--- cli/src/omnipresence/Who.py
+++ cli/src/omnipresence/Who.py
@@ -2,11 +2,24 @@ import os
 import sys
 import requests
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Who:
 
     def __init__(self):
         self.cwd = os.getcwd()
-        user_list = self.__get_user_list(
+        self.user = os.getenv('GITHUB_USER')
+        user_list = self.__get_user_list()
+
+    def __get_user_list():
+        actives = requests.post(
+            f"{os.getenv('API_HOST'):{os.getenv('API_PORT')}/v1/omnipresence/",
+            data = {
+                "cwd": self.cwd
+            }
+        print(actives.json())
 
 def cmd():
     Who()
