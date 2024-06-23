9f07d1372c0b9125a91286708f1ac1da9f253558
dluman <dluman@allegheny.edu>
2024-06-23 00:53:17 -0400

    Updating with some more co-mingled updates, but necessary for the cause

--- cli/src/narrator/Checkpoint.py
+++ cli/src/narrator/Checkpoint.py
@@ -1,6 +1,12 @@
 import os
 import json
 
+from dotenv import load_dotenv
+
+load_dotenv()
+print(os.environ)
+root_dir = os.path.expanduser(os.getenv('FILE_SYSTEM') + os.getenv('RepositoryName'))
+
 from .Path import *
 
 def exists(filepaths: list) -> bool:
@@ -11,19 +17,19 @@ def exists(filepaths: list) -> bool:
 
 def set_flag(flag: str, val:int = 1) -> None:
   flags = {}
-  if not exists([".flags"]):
-    with open(".flags", "w+") as fh:
+  if not exists([os.path.expanduser(f"{root_dir}/.flags")]):
+    with open(f"{root_dir}/.flags", "w+") as fh:
       fh.write("{}")
-  with open(".flags", "r+") as fh:
+  with open(f"{os.getenv('root_dir')}/.flags", "r+") as fh:
     flags = json.load(fh)
   flags[flag] = val
-  with open(".flags", "w") as fh:
+  with open(f"{os.getenv('root_dir')}/.flags", "w") as fh:
     json.dump(flags, fh, indent=2)
 
 def check_flag(flag: str):
   flags = {}
   try:
-    with open(".flags", "r+") as fh:
+    with open(f"{root_dir}/.flags", "r+") as fh:
       flags = json.load(fh)
     return flags[flag]
   except:
